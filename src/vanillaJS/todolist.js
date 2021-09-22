4c72a2a823794d8f1a2486ba81a1fdf96ca9d8a3
je0489 <je0489@naver.com>
2021-09-23 01:47:57 +0900

    Feat: add To-do delete event

--- src/vanillaJS/todolist.js
+++ src/vanillaJS/todolist.js
@@ -5,34 +5,48 @@ const input = document.querySelector("input");
 const ul = document.querySelector("ul");
 
 const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
 
 const reducer = ( state = [], action ) => {
-    const { type, text } = action;
-    if( type === ADD_TODO )
-        return [{ text }, ...state];
-    return state;
+    const { type, text, id: id1 } = action;
+    switch( type ) {
+        case ADD_TODO:
+            return [{ id: Date.now(), text }, ...state];
+        case DELETE_TODO:
+            return state.filter( ({ id }) => id !== +id1 );
+        default:
+            return state;
+    }
 }
-
-const store = createStore( reducer );
 
 const showTodos = () => {
     const todos = store.getState();
     ul.innerHTML = "";
-    todos.forEach( todo => {
+    todos.forEach( ({ text, id }) => {
         const li = document.createElement("li");
-        li.innerText = todo.text;
+        const btn = document.createElement("button");
+        btn.innerText = "DELETE";
+        btn.addEventListener( "click", handleTodoDelete );
+        li.id = id;
+        li.innerText = `${text} `;
+        li.appendChild(btn);
         ul.appendChild(li);
     });
 }
 
-store.subscribe( () => console.log( store.getState() ) );
-store.subscribe( showTodos );
-
-const onSubmit = event => {
+const handleTodoSubmit = event => {
     event.preventDefault();
     const todo = input.value;
     input.value = "";
     store.dispatch( { type: ADD_TODO, text: todo } );
 }
 
-form.addEventListener( "submit", onSubmit ); 
+const handleTodoDelete = event => {
+    const id = event.target.parentNode.id;
+    store.dispatch( { type: DELETE_TODO, id });
+}
+
+const store = createStore( reducer );
+store.subscribe( showTodos );
+
+form.addEventListener( "submit", handleTodoSubmit ); 
