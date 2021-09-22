8ee581ffa7e2c2bed5a84c372ee02fc0fa34907b
je0489 <je0489@naver.com>
2021-09-23 00:47:19 +0900

    Fix: with vanilla-redux

--- src/vanillaJS/todolist.js
+++ src/vanillaJS/todolist.js
@@ -1,18 +1,38 @@
+import { createStore } from "redux";
+
 const form = document.querySelector("form");
 const input = document.querySelector("input");
 const ul = document.querySelector("ul");
 
-const createTodos = val => {
-    const li = document.createElement("li");
-    li.innerText = val;
-    ul.appendChild(li);
+const ADD_TODO = "ADD_TODO";
+
+const reducer = ( state = [], action ) => {
+    const { type, text } = action;
+    if( type === ADD_TODO )
+        return [{ text }, ...state];
+    return state;
 }
+
+const store = createStore( reducer );
+
+const showTodos = () => {
+    const todos = store.getState();
+    ul.innerHTML = "";
+    todos.forEach( todo => {
+        const li = document.createElement("li");
+        li.innerText = todo.text;
+        ul.appendChild(li);
+    });
+}
+
+store.subscribe( () => console.log( store.getState() ) );
+store.subscribe( showTodos );
 
 const onSubmit = event => {
     event.preventDefault();
     const todo = input.value;
     input.value = "";
-    createTodos( todo );
+    store.dispatch( { type: ADD_TODO, text: todo } );
 }
 
 form.addEventListener( "submit", onSubmit ); 
