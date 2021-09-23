ae9b94158e3f3e635e654b6181370043d5f691d8
je0489 <je0489@naver.com>
2021-09-24 00:27:00 +0900

    Feat: use toolkit's createReducer instead of reducer

--- src/store.js
+++ src/store.js
@@ -1,22 +1,20 @@
 import { createStore } from "redux";
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
 const addTodo = createAction("ADD_TODO");
 const delTodo = createAction("DELETE_TODO");
 
-const reducer = (state = [], action) => {
-  // toolkit 사용시 action의 props는 type과 payload 뿐
-  const { type, payload } = action;
-  console.log(action);
-  switch (type) {
-    case addTodo.type:
-      return [{ id: Date.now(), text: payload }, ...state];
-    case delTodo.type:
-      return state.filter(({ id }) => id !== +payload);
-    default:
-      return state;
-  }
-};
+// createReducer( initial state: [], reducer function ): State mutate 가능
+// 1. new State인 경우 return
+// 2. state mutate한 경우 void
+// ㄴ1, 2가 가능한 이유: Immer is in background. It takes mutations and creates a new state.
+const reducer = createReducer([], {
+  [addTodo]: (state, { payload }) => {
+    state.unshift({ text: payload, id: Date.now() });
+    // return [{ id: Date.now(), text: payload }, ...state]; < toolkit 적용 전 state mutate가 불가능했었음
+  },
+  [delTodo]: (state, { payload }) => state.filter(({ id }) => id !== +payload),
+});
 
 const store = createStore(reducer);
 
